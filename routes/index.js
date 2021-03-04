62da96872e17de870128532a657179d641158ffd
stephenson-heritage <LStephenson@cegep-heritage.qc.ca>
2021-03-04 13:18:28 -0500

    some initial changes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.send("home");
 });
-
+router.get('/h', function (req, res, next) {
+  res.render('index', { title: 'h' });
+});
 module.exports = router;
